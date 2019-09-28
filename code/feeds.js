3265facb41a4c7ee0c7e81fcf9f1cac913cfc743
Jamie Dawson <jadawson@e1z3r1p14.42.us.org>
2019-09-28 14:20:57 -0700

    All files for VUX World

---
+++ code/feeds.js
@@ -0,0 +1,13 @@
+// Add RSS feeds below
+// Url: Link to RSS feed xml
+// urlText: The text that is displayed for attribution link
+// Text: What users see in feed selection list
+// Tags: What the user can say to automatically call certain feeds
+module.exports = module.exports = [
+  {
+    url: 'https://feed.pippa.io/public/shows/vux-world',
+    urlText: 'Click here to see the full podcast',
+    text: 'VUX World',
+    tags: []
+  }
+]
